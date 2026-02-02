922c172da53311335f5eeda9eb07b51cea2b53b1
SeanPMilligan <150982103+spm01@users.noreply.github.com>
2026-02-01 16:59:22 -0800

    Add files via upload
    Newest additions include full evaluation of PnL for FY2023.
    Next step is to test on FY2024 data?
    Also probably need to tune K parameter on 2023 data for application on 2024 data.

--- pairstrade.py
+++ pairstrade.py
@@ -31,6 +31,7 @@ df_wide = (
 #create new column for price ratio
 #IRL probably want a price spread instead of price ratio
 #test price spread of ~(6/17) --> normally probably want rolling price spread
+#fractional changes make huge difference in performance (desmos graphic calc had ~0.01 difference with giant misses)
 df_price = df_wide.with_columns(
     (pl.col('KO US EQUITY') / pl.col('PEP US EQUITY')).alias('KO / PEP Price Ratio'),
 )
@@ -50,10 +51,10 @@ df_error = df_ratio.with_columns(
 )
 #print(df_error)
 
-#create new column with absolute value 30 day rolling avg deviation
+#create new column with absolute value 30 day rolling std deviation
 #using this column to create boundaries for the DAILY price ratio
 df_noise = df_error.with_columns(
-    (pl.col('Deviation').rolling_mean(window_size=30).abs().alias('Moving 30 Day Deviation Avg'),
+    (pl.col('Deviation').rolling_std(window_size=30).abs().alias('Moving 30 Day Std Deviation'),
     pl.col('date').cast(pl.Date))
 )
 #print(df_noise)
@@ -61,24 +62,63 @@ df_noise = df_error.with_columns(
 #create 2 new columns: upper band and lower band
 #this is to measure whether price movements are actually substantial or not
 df_boundary = df_noise.with_columns(
-    ((pl.col('Moving 30 Day Price Ratio Avg') + (pl.col('Moving 30 Day Deviation Avg') * 1.5)).alias('Upper Bound')),
-    (((pl.col("Moving 30 Day Price Ratio Avg")) - (pl.col('Moving 30 Day Deviation Avg') * 1.5)).alias('Lower Bound')),
+    ((pl.col('Moving 30 Day Price Ratio Avg') + (pl.col('Moving 30 Day Std Deviation') * 1.5)).alias('Upper Bound')),
+    (((pl.col("Moving 30 Day Price Ratio Avg")) - (pl.col('Moving 30 Day Std Deviation') * 1.5)).alias('Lower Bound')),
      pl.col('date').cast(pl.Date)
 )
 print(df_boundary)
+#typically it would be good to tune the K parameter based on our given target metrics (Sharpe's ratio/drawdowns etc) 
 
+#define our positions for the trades
+#-1 we short the relationship with the expectation that it will fall back to normal
+#1 we buy the relationship with the hope it will rise back to normal
 df_conditional = df_boundary.with_columns(
-    pl.when(pl.col("KO / PEP Price Ratio") > pl.col('Upper Bound')).then(pl.lit(1))
-    .when(pl.col("KO / PEP Price Ratio") < pl.col('Lower Bound')).then(pl.lit(-1))
+    pl.when(pl.col("KO / PEP Price Ratio") > pl.col('Upper Bound')).then(pl.lit(-1))
+    .when(pl.col("KO / PEP Price Ratio") < pl.col('Lower Bound')).then(pl.lit(1))
     .otherwise(pl.lit(0))
-    .alias("Price Indicator")        
+    .alias("Target Position")        
 )
-
 #print(df_conditional)
 
+#computing simulate PnL for 2023
+df_pnl = df_conditional.with_columns(
+    (pl.col('KO US EQUITY').pct_change().alias('Ret_KO')),
+    (pl.col('PEP US EQUITY').pct_change().alias('Ret_PEP')),
+    (pl.col('Target Position').shift(1).alias('Position Held'))
+).with_columns(
+    ((pl.col("Ret_KO") - pl.col('Ret_PEP')) * pl.col('Position Held')).alias('Strategy_Daily_Return')
+).with_columns(
+    pl.col('Strategy_Daily_Return').fill_null(0).cum_sum().alias('Cumulative_PnL')
+).drop_nulls()
+
+#df_count = df_pnl.select(pl.col('Position Held').value_counts())
+#print(df_count)
+#with K = 1.5, 1 values = 27, 0 values = 138, 1 values = 27
+
+#print(df_pnl)
+#cumulative PnL is ~4.7% for FY2023
+print(df_pnl.select(['date', 'KO / PEP Price Ratio', 'Position Held', 'Strategy_Daily_Return', 'Cumulative_PnL']))
+
+
+
+
+'''
+df_entry = df_conditional.with_columns(
+    pl.when(
+        (pl.col('Target Price').shift(1) == 0) &
+        (pl.col('Target Price').is_in([1,-1]))
+    )
+    .then(1)
+    .otherwise(0)
+    .alias('Entry')
+)
+print(df_entry)
+'''
+
 #checking how many times the KO / PEP Price Ratio exceeded current boundaries
-df_count = df_conditional.select(pl.col('Price Indicator').value_counts())
-print(df_count)
+#df_count = df_conditional.select(pl.col('Target Price').value_counts())
+#print(df_count)
+#with K = 1.5, -1 values = 48, 0 values = 164, 1 values = 38
 
 #melt polars to long format for charting
 df_long = df_error.unpivot(
